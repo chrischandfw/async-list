0d9f66416284c10f250668e0c0f35971165232af
chris_chan <chrischandfw@gmail.com>
2022-03-22 18:49:53 -0500

    added async function for displaying retainers in app js file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // import functions and grab DOM elements
 import { getGames } from './fetch-utils.js';
 import { renderGame } from './render-utils.js';
+
+import { getRetainers } from './fetch-utils.js';
+import { renderRetainers } from './render-utils.js';
+
 // let state
 
 // set event listeners 
@@ -9,9 +13,11 @@ import { renderGame } from './render-utils.js';
   // update DOM to reflect the new state
 
 const gamesListEl = document.querySelector('.games-list');
+const retainersListEl = document.querySelector('.retainers-list');
 
 window.addEventListener('load', async () => {
     fetchAndDisplayGames();
+    fetchAndDisplayRetainers();
 });
 
 
@@ -21,5 +27,14 @@ async function fetchAndDisplayGames() {
     for (let game of games) {
         const gameEl = renderGame(game);
         gamesListEl.append(gameEl);
+    }
+}
+
+async function fetchAndDisplayRetainers() {
+    const retainers = await getRetainers();
+
+    for (let retainer of retainers) {
+        const retainerEl = renderRetainers(retainer);
+        retainersListEl.append(retainerEl);
     }
 }
