d86d55ef0f512d0574e977bd7de6bd5fd0dbafe0
chris_chan <chrischandfw@gmail.com>
2022-03-22 15:55:05 -0500

    added render game function in render file

---
+++ render-utils.js
@@ -0,0 +1,28 @@
+export function renderGame(game) {
+    const gameEl = document.createElement('div');
+    const titleEl = document.createElement('p');
+    const yearEl = document.createElement('p');
+    const ratingEl = document.createElement('p');
+    const platformEl = document.createElement('ol');
+	
+    gameEl.classList.add('game');
+
+    titleEl.textContent = game.title;
+    yearEl.textContent = game.year_released;
+    ratingEl.textContent = game.rating;
+    platformEl.textContent = game.platform;
+
+    for (let gamingPlatform of game.platform) {
+        const li = document.createElement('li');
+
+        li.textContent = gamingPlatform;
+        platformEl.appendChild(li);
+    }
+
+    gameEl.append(
+        titleEl,
+        yearEl,
+        ratingEl,
+        platformEl
+    ); 
+}
